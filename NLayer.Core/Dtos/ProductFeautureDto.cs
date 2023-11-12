eced0fb21109c1c4480eac977113c0670617edb0
Enes Tunahan <enesstunahan@gmail.com>
2023-11-12 20:47:55 +0300

    Dtolar oluşturuldu

---
+++ NLayer.Core/Dtos/ProductFeautureDto.cs
@@ -0,0 +1,12 @@
+﻿namespace NLayer.Core.Dtos
+{
+    public class ProductFeautureDto
+    {
+        public int Id { get; set; }
+        public string Color { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public int ProductId { get; set; }
+    }
+}
