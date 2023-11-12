6316797f8677fd41630faa3c5351bf777494fdb4
Enes Tunahan <enesstunahan@gmail.com>
2023-11-12 15:16:49 +0300

    AppDbContext has been created

---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Models;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+    }
+}
