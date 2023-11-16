8bf9fb06edee7bfda4548e134f7ec50afa6dab19
Enes Tunahan <enesstunahan@gmail.com>
2023-11-16 23:08:10 +0300

    FluentValidation Library has been added and ProductDtoValidator has been creeated

---
+++ NLayer.Services/Validations/ProductDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using NLayer.Core.Dtos;
+
+namespace NLayer.Services.Validations
+{
+    public class ProductDtoValidator : AbstractValidator<ProductDto>
+    {
+        public ProductDtoValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("{PropertyName} is required")
+                .NotNull().WithMessage("{PropertyName} is required");
+
+            RuleFor(x => x.Price).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater than 0");
+            RuleFor(x => x.Stock).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater than 0");
+            RuleFor(x => x.CategoryId).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater than 0");
+        }
+    }
+}
