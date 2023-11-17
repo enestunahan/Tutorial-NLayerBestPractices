2c96b324ec0eefe8493b137892ef4a5f35960532
Enes Tunahan <enesstunahan@gmail.com>
2023-11-17 23:51:46 +0300

    Filter eklenerek hata alındığı esnada fluentvalidation default modeli dönmek yerine customresponsedto dönülüyor

--- NLayer.API/Controllers/CategoriesController.cs
+++ NLayer.API/Controllers/CategoriesController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NLayer.API.Filters;
 using NLayer.Core.Service;
 
 namespace NLayer.API.Controllers
 {
+    [ValidateFilterAttribute]
     public class CategoriesController : CustomBaseController
     {
         private readonly ICategoryService _categoryService;
